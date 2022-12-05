e05da2de30593e4c38de189fcfba16ba723d2429
Jordi NodeJS <jordi.nodejs@gmail.com>
2022-12-05 16:29:28 +0000

    firtcommit

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import { ChangeEvent, useEffect, useState } from "react";
+import "./App.css";
+import Shape, { Size, Color } from "./components/Shape";
+
+function App() {
+  const initialSize: Size = {
+    triangle: 70,
+    circle: 60,
+    square: 30
+  }
+  const initialColor: Color = {
+    triangle: '#555',
+    circle: '#554040',
+    square: '#555222'
+  }
+  const [size, setSize] = useState<Size>(initialSize)
+  const [globalColors, setGlobalcolors] = useState<Color>(initialColor)
+  useEffect(() => {
+    console.log(size)
+    console.log(globalColors)
+    const { triangle, circle, square } = size
+    if (triangle === circle && circle === square) alert('los tres tiene el mismo tamaño')
+
+  }, [size])
+
+  const handleSize = ({ target: { value, name } }: ChangeEvent<HTMLInputElement>): void => {
+    console.log(name, value)
+    let newState = { [name]: +value }
+    newState = { ...newState }
+    setSize((prev: Size) => ({ ...prev, ...newState }))
+  }
+  return (
+    <div className="App">
+      <Shape stateColors={[globalColors,setGlobalcolors]}  shape='triangle' size={size} handleChange={handleSize} />
+      <Shape stateColors={[globalColors,setGlobalcolors]} shape='circle' size={size} handleChange={handleSize} />
+      <Shape stateColors={[globalColors,setGlobalcolors]} shape='square' size={size} handleChange={handleSize} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Shape.tsx
@@ -0,0 +1,88 @@
+import { useState, ChangeEvent, ChangeEventHandler, useEffect } from "react";
+export interface Size {
+  triangle: number;
+  circle: number;
+  square: number;
+}
+export type Color = {
+  triangle: string;
+  circle: string;
+  square: string;
+};
+type Props = {
+  stateColors: any;
+  shape: string;
+  size: Size;
+  colorProp?: string;
+  handleChange: ChangeEventHandler;
+};
+
+const Shape = ({
+  stateColors: [globalColors, setGlobalcolors],
+  shape,
+  size,
+  colorProp = "#551541",
+  handleChange,
+}: Props) => {
+  const [color, setColor] = useState(colorProp);
+  const { triangle, circle, square } = size;
+
+  const divStyle = {
+    display: "flex",
+    flexDirection: "column" as "column",
+    justifyContent: "center",
+    marginBottom: "3rem",
+  };
+  const triangleStyles = {
+    width: 0,
+    height: 0,
+    borderLeft: `${triangle / 2}px solid transparent`,
+    borderRight: `${triangle / 2}px solid transparent`,
+    borderBottom: `${triangle}px solid ${color}`,
+    marginTop: "1rem",
+  };
+  const circleStyle = {
+    height: `${circle}px`,
+    width: `${circle}px`,
+    backgroundColor: `${color}`,
+    borderRadius: "50%",
+  };
+  const squareStyle = {
+    height: `${square}px`,
+    width: `${square}px`,
+    backgroundColor: `${color}`,
+  };
+  useEffect(() => {
+    const global_color = { [shape]: color };
+    const { triangle, circle, square } = globalColors;
+    if (triangle === circle && circle === square)
+      alert("los tres tiene el mismo color");
+    setGlobalcolors((prev: any) => ({ ...prev, ...global_color }));
+  }, [color]);
+
+  const handleColor = ({
+    target: { value },
+  }: ChangeEvent<HTMLInputElement>): void => setColor(value);
+
+  const shapeStyle =
+    shape == "triangle"
+      ? triangleStyles
+      : shape == "circle"
+      ? circleStyle
+      : squareStyle;
+
+  return (
+    <div style={divStyle}>
+      <div>
+        <label>Elige un color:</label>
+        <input onChange={handleColor} type="color" value={color} />
+      </div>
+      <div>
+        <label>Elige el tamaño:</label>
+        <input name={shape} onChange={handleChange} type="number" />
+      </div>
+      <div style={shapeStyle}></div>
+    </div>
+  );
+};
+export default Shape;
