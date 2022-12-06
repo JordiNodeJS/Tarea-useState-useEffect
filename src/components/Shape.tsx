6622ab0ab8fa145ca841a6fa4e0de088581edeca
Kevin Mamaqi Kapllani <kevin.mamaqi@90poe.io>
2022-12-06 06:28:33 +0000

    Primera revisión

--- src/components/Shape.tsx
+++ src/components/Shape.tsx
@@ -1,36 +1,42 @@
-import { useState, ChangeEvent, ChangeEventHandler, useEffect } from "react"
+import { useState, ChangeEvent, ChangeEventHandler, useEffect } from "react";
 export interface Size {
-  triangle: number
-  circle: number
-  square: number
+  triangle: number;
+  circle: number;
+  square: number;
 }
 export type Color = {
-  triangle: string
-  circle: string
-  square: string
-}
+  triangle: string;
+  circle: string;
+  square: string;
+};
 type Props = {
-  stateColors: any
-  shape: string
-  size: Size
-  handleChange: ChangeEventHandler
-}
+  stateColors: any;
+  shape: string;
+  size: Size;
+  handleChange: ChangeEventHandler;
+};
 
+// Mejor crear 3 componentes Triangle, Circle y Square
+// Usar SVG para las formas como componentes React
+// Validar las props
 const Shape = ({
   stateColors: [globalColors, setGlobalcolors],
   shape,
   size,
   handleChange,
 }: Props) => {
-  const [color, setColor] = useState(asignColor(shape))
-  const { triangle, circle, square } = size
+  const [color, setColor] = useState(asignColor(shape));
+  const { triangle, circle, square } = size;
 
+  // Declararlo fuera, o usar styled-components, pero no es necesario para el ejercicio.
   const divStyle = {
     display: "flex",
     flexDirection: "column" as "column",
     justifyContent: "center",
     marginBottom: "3rem",
-  }
+  };
+
+  // Al escoger una estrategia por CSS y agrupar todo en un componente demasiado generico te ves obligado al código que tienes aquí.
   const triangleStyles = {
     width: 0,
     height: 0,
@@ -38,47 +44,49 @@ const Shape = ({
     borderRight: `${triangle / 2}px solid transparent`,
     borderBottom: `${triangle}px solid ${color}`,
     marginTop: "1rem",
-  }
+  };
   const circleStyle = {
     height: `${circle}px`,
     width: `${circle}px`,
     backgroundColor: `${color}`,
     borderRadius: "50%",
-  }
+  };
   const squareStyle = {
     height: `${square}px`,
     width: `${square}px`,
     backgroundColor: `${color}`,
-  }
+  };
 
   useEffect(() => {
-    const global_color = { [shape]: color }
-    const { triangle, circle, square } = globalColors
+    const global_color = { [shape]: color };
+    const { triangle, circle, square } = globalColors;
     if (triangle === circle && circle === square)
-      alert("los tres tiene el mismo color")
-    setGlobalcolors((prev: Color) => ({ ...prev, ...global_color }))
-  }, [color])
+      alert("los tres tiene el mismo color");
+    setGlobalcolors((prev: Color) => ({ ...prev, ...global_color }));
+  }, [color]);
 
   function asignColor(shape: string): string {
-    const { triangle, circle, square } = globalColors
-    return shape == "triangle" ? triangle : shape == "circle" ? circle : square
+    const { triangle, circle, square } = globalColors;
+    return shape == "triangle" ? triangle : shape == "circle" ? circle : square;
   }
 
   const handleColor = ({
     target: { value },
-  }: ChangeEvent<HTMLInputElement>): void => setColor(value)
+  }: ChangeEvent<HTMLInputElement>): void => setColor(value);
 
   const shapeStyle =
     shape == "triangle"
       ? triangleStyles
       : shape == "circle"
       ? circleStyle
-      : squareStyle
+      : squareStyle;
 
   return (
     <div style={divStyle}>
       <div>
+        {/* No tiene etiqueta de accecsibilidad */}
         <label>Elige un color:</label>
+        {/* No tiene ID o name */}
         <input onChange={handleColor} type="color" value={color} />
       </div>
       <div>
@@ -87,6 +95,6 @@ const Shape = ({
       </div>
       <div style={shapeStyle}></div>
     </div>
-  )
-}
-export default Shape
+  );
+};
+export default Shape;
